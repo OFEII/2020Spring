ab715f1d110dcb4b5b85e7154987ede2b2d73f06
OFEII <2282252066@qq.com>
2020-03-09 11:24:53 +0800

    +nowcode in 2020春招 腾讯

--- tx/tx1.js
+++ tx/tx1.js
@@ -1,13 +1,5 @@
 //1.格式化金钱，每千分位加逗号，需要注意的是：const target = '123456789' ，然后通过target.format()这样子调用
-// var a = 1123456789;
-// // var b = a.toFixed(0).replace(/(\d{1,3})(,\d{3})*/g, '$1,');
-// function toThousands(num) {
-//     var b= (num || 0).toString().replace(/(\d)(?=(?:\d{3})+$)/g, '$1,');
-//     console.log(b);
 
-// }
-// toThousands(1234567891);
-// 
 function formatData(num){
 	num += '';
 	if (!num.includes('.')) num += '.';
@@ -16,6 +8,6 @@ function formatData(num){
 	}).replace(/\.$/, '');
 
 };
-console.log(formatData(1234567891.444444));
+console.log(formatData(1234567891));
 
 
